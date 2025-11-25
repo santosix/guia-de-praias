b90fd84d43b7b655584e1664506086257d5ad147
santosix <151579114+santosix@users.noreply.github.com>
2025-11-25 16:33:49 -0300

    Add files via upload

---
+++ docs/details.js
@@ -0,0 +1,21 @@
+const params = new URLSearchParams(window.location.search);
+const nomePraia = params.get("nome");
+
+fetch("data.json")
+  .then(res => res.json())
+  .then(dados => {
+    const praia = dados.find(p => p.nome === nomePraia);
+
+    if (!praia) {
+      document.body.innerHTML = "<h1>Praia não encontrada</h1>";
+      return;
+    }
+
+    document.getElementById("praia-nome").textContent = praia.nome;
+    document.getElementById("praia-desc").textContent = praia.descricao;
+    document.getElementById("praia-img").src = praia.imagem;
+
+    document.getElementById("praia-mapa").src =
+      `https://SEU-BACKEND-PUBLICO.onrender.com/map?lat=${lat}&lon=${lon}`;
+  });
+
